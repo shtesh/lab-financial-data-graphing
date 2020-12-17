481b7c32b9f40ca62a0858a8f29d07ebe29b2614
shtesh <vshtefanuka@gmail.com>
2020-12-17 23:22:13 +0100

    Done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,69 +1,84 @@
-const ctx = document.getElementById('canvas').getContext('2d');
-let fromDate = " ";
-let toDate = new Date().toISOString().slice(0,10);
-let startDay = new Date();
-pastDate = startDay.getDate() - 30;
-startDay.setDate(pastDate);
-fromDate = startDay.toISOString().slice(0,10);
+const API_URL = 'http://api.coindesk.com/v1/bpi/historical/close.json';
 
-const url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`;
-document.getElementById('input-start-date').value = fromDate;
-document.getElementById('input-end-date').value = toDate;
+const submitBtn = document.querySelector('#submit');
+const startDateInput = document.querySelector('#start');
+const endDateInput = document.querySelector('#end');
+const currencySelect = document.querySelector('#curr');
+const maxEle = document.querySelector('#max');
+const minEle = document.querySelector('#min');
 
-const getFinancialData = urlApi => {
-    axios
-    .get(urlApi, {crossdomain: true})
-    .then(response => {
-        const dates = Object.keys(response.data.bpi);
-        const values = dates.map(date => response.data.bpi[date]);
-        document.getElementById('min').innerHTML = Math.min(...values);
-        document.getElementById('max').innerHTML = Math.max(...values);
+// ITERATION 1 - Get data (initial API call when loading the page)
+const getInitialAPIData = async () => {
+    const response = await axios.get(API_URL);
+    const data = response.data.bpi;
+    // console.log('data :>> ', data);
 
-        const data = {
-            labels: [...dates],
-            datasets: [{
-                label: 'Current price',
-                data: values,
-                borderWidth: 1.5
-            }]
-        };
+    // ITERATION 2
+  printTheChart(data);
+};
+getInitialAPIData();
 
-        let lineChart = new Chart(ctx, {
-            type: 'line',
-            data: data,
-        })
-        .catch(err => {
-            console.log(err);
-        });
+// ITERATION 2 - Create a chart
+const printTheChart = (data, currency) => {
+    if(!currency) currency = 'USD';
+  
+    let ctx = document.getElementById('bitcoin-chart').getContext('2d');
+  
+    let dates = Object.keys(data);
+    let bitcoinValues = Object.values(data);
+    let formattedData = {
+      labels: dates,
+      datasets: [{
+        label: `Bitcoin value in ${currency}`,
+        data: bitcoinValues,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        borderWidth: 1
+      }]
+    };
+
+    new Chart(ctx, {
+        type: 'line',
+        data: formattedData
     });
+
+// ITERATION 5 (BONUS) - Max/Min Values
+  printMinAndMaxValues(data, currency); 
 };
 
-getFinancialData(url);
+// ITERATION 3 & ITERATION 4 (BONUS) - Dates & currency filters
+const getAPIDataWithFilters = async (filters) => {
+    const { startDate, endDate, currency } = filters;
+  
+    const response = await axios.get(`${API_URL}?start=${startDate}&end=${endDate}&currency=${currency}`);
+    const data = response.data.bpi;
+    
+    printTheChart(data, currency);
+};
 
-document.getElementById('input-start-date').addEventListener('change', () => {
-    let startDate = document.getElementById('input-start-date').value;
-    let endDate = document.getElementById('input-end-date').value;
-    let currency = document.getElementById('input-currency').value;
-    let url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`;
-    getFinancialData(url);
+// ITERATION 3 & ITERATION 4 (BONUS) - Event listener on the filters submit button
+submitBtn.addEventListener('click', () => {
+    const startDate = startDateInput.value;
+    const endDate = endDateInput.value;
+    const currency = currencySelect.value;
+  
+    if(!startDate || !endDate) {
+      alert('you must complete the 2 dates');
+      return;
+    }
+  
+    getAPIDataWithFilters({ startDate, endDate, currency });
 });
 
-document.getElementById('input-end-date').addEventListener('change', () => {
-    let startDate = document.getElementById('input-start-date').value;
-    let endDate = document.getElementById('input-end-date').value;
-    let currency = document.getElementById('input-currency').value;
-    let url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`;
-    getFinancialData(url);
-});
+// ITERATION 5 (BONUS) - Max/Min Values
+const printMinAndMaxValues = (data, currency) => {
+    const bitcoinValues = Object.values(data);
+    const minValue = Math.min(...bitcoinValues).toFixed(2);
+    const maxValue = Math.max(...bitcoinValues).toFixed(2);
+    minEle.innerHTML = `${currency} ${minValue}`;
+    maxEle.innerHTML = `${currency} ${maxValue}`;
+};
 
-document.getElementById('input-currency').addEventListener('change', () => {
-    let startDate = document.getElementById('input-start-date').value;
-    let endDate = document.getElementById('input-end-date').value;
-    let currency = document.getElementById('input-currency').value;
-    let url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`;
-    console.log(url);
-    getFinancialData(url);
-});
 
 
 
